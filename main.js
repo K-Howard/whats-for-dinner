b85c704210c9cb388406ef84cd0768b39b428849
Kyle Howard <kylehoward1228@yahoo.com>
2022-02-16 14:23:06 -0700

    add variables and other

---
+++ main.js
@@ -0,0 +1,66 @@
+var letsCook = document.querySelector(".lets-cook")
+document.querySelector()
+document.querySelector()
+document.querySelector()
+
+
+
+letsCook.addEventListener('click', cook)
+.addEventListener('click', )
+.addEventListener('click', )
+.addEventListener('click', )
+
+function cook() {
+ 
+}
+
+
+sides = [
+'Miso Glazed Carrots',
+'Coleslaw',
+'Garden Salad',
+'Crispy Potatoes',
+'Sweet Potato Tots',
+'Coconut Rice',
+'Caeser Salad',
+'Shrimp Summer Rolls',
+'Garlic Butter Mushrooms',
+'Hush Puppies'
+]
+
+mains = [
+  'Spaghetti and Meatballs',
+  'Pineapple Chicken',
+  'Shakshuka',
+  'Thai Yellow Curry',
+  'Bibimbap',
+  'Chicken Parmesean',
+  'Butternut Squash Soup',
+  'BBQ Chicken Burgers',
+  'Ramen',
+  'Empanadas',
+  'Chicken Fried Rice',
+  'Sheet Pan Fajitas',
+  'Margarita Pizza'
+]
+
+desserts = [
+  'Apple Pie',
+  'Lemon Meringue Pie',
+  'Black Forest Cake',
+  'Banana Bread',
+  'Peach Cobbler',
+  'Cheesecake',
+  'Funfetti Cake',
+  'Baklava',
+  'Flan',
+  'Macarons',
+  'Macaroons',
+  'Chocolate Cupcakes',
+  'Pavlova',
+  'Pumpkin Pie',
+  'Key Lime Pie',
+  'Tart Tatin',
+  'Croissants',
+  'Eclairs',
+]
